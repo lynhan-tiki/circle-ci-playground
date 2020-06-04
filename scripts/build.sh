5b3232781c97c7d5b63916248ec232baf08f3bae
Ly Nhan <lythanhnhan27294@gmail.com>
2020-06-04 15:27:59 +0700

    test build

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ function buildByApp() {
   case $1 in
   "mweb")
     echo "trigger build mweb"
-    #circleci-agent step halt
+    circleci-agent step halt
     ;;
   "miniapp")
     echo "trigger build miniapp"

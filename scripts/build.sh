ab55f30528056d0794dcd49caf21a3cdfa82f5e4
Ly Nhan <lythanhnhan27294@gmail.com>
2020-06-04 14:53:39 +0700

    a

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,26 @@
-echo "run script build"
-circleci
-echo "run second echo"
+#!/usr/bin/env bash
+
+BRANCH="develop"
+APP=$1
+set -e
+
+function getApp() {
+    APP="mweb"
+}
+
+function buildByApp() {
+  case $1 in
+  "mweb")
+    echo "trigger build mweb"
+    ;;
+  "miniapp")
+    echo "trigger build miniapp"
+    ;;
+  *)
+      exit 1
+      echo "[ MINIAPP|MWEB ] are support"
+    ;;
+  esac
+}
+#getApp
+buildByApp $APP
